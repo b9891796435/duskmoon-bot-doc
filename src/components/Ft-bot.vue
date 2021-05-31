ae580fb3d5e7bcf0485beee1743fe3a95bb16d59
b9891796435 <1362282435@qq.com>
2021-05-31 14:26:31 +0800

    initialize this repo
    just to initialize.

---
+++ src/components/Ft-bot.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1>Fimtale Bot指令帮助</h1>
+    <p>
+        所有ft指令均以/ft开头，指令各参数间以空格隔开。
+    </p>
+    <ul>
+        <li>
+            同人金句quote：<br/>
+            获取一条主页最下方的同人金句<code>/ft quote</code>,该指令无参数。
+        </li>
+        <li>
+            公告列表anno：<br/>
+            获取当前主页展示的公告列表<code>/ft anno</code>,该指令无参数。
+        </li>
+        <li>
+            编辑推荐er：<br/>
+            从当前主页的编辑推荐中随机抽取一篇小说<code>/ft er</code>，该指令无参数。
+        </li>
+        <li>
+            近日热门np：<br/>
+            从当前主页的近日热门中随机抽取一篇小说<code>/ft np</code>，该指令无参数。
+        </li>
+        <li>
+            最近更新nu：<br/>
+            从当前主页的最近更新中随机抽取一篇小说<code>/ft nu</code>，该指令无参数。
+        </li>
+        <li>
+            小说搜索search：<br/>
+            调用ft搜索功能搜索关键字，<code>/ft search keyword</code>，该指令参数为keyword，参数意义为需要搜索的关键字。指令只会返回搜索结果前两篇小说。
+            例：要搜索“图书馆里的幽灵公主”，需要输入<code>/ft search 图书馆里的幽灵公主</code>
+        </li>
+        <p>以上指令除同人金句均有显示完整列表变种，但由于超过一定字数企鹅娘会吃掉整条消息，所以无法使用。</p>
+    </ul>
+    <p>
+        
+    </p>
+</template>
+
+<script>
+export default {
+  name: "FtBot",
+};
+</script>
+
+<style scoped>
+* {
+    text-align: left;
+    line-height: 1.6em;
+    margin: 1.2em 0 -1.2em;
+    padding-bottom: 1.2em;
+    position: relative;
+    z-index: 1;
+}
+code{
+    padding-bottom: 0;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>黄昏之月Bot文档</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      这里�?黄昏之月Bot文档，�?�载了Bot指令。Bot基于mirai-qq，使用garia-project编写。文档使用Vue框架<br/>
+      <a
+        href="https://qm.qq.com/cgi-bin/qm/qr?k=OpSmBL9fOIiBeQE0ZsxPn__9n94T5Xev&noverify=0"
+        >添加开发者QQ好友</a
+      >
+      <br />
+      <a
+        href="https://qm.qq.com/cgi-bin/qm/qr?k=-r-jtK0E-5_LFWH8Ru8FpiK-qLhl6Toq&noverify=0"
+        >添加BOT QQ好友</a
+      >
+      <br/>
+      注意bot单条消息字数超过100字�?�或64字后很有�?能�??吞，所以�?�果指令发出后没有回应，那大概率�?�?企鹅娘吞掉了（确�?
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
